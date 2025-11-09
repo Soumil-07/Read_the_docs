37439b08a9c8e31afdbd197c3215d26884407b20
Soumil Paranjpay <soumil.paranjpay@gmail.com>
2025-11-08 16:47:51 -0800

    revert dft.cpp

--- project_files/project3/dft_1024_precomputed/dft.cpp
+++ project_files/project3/dft_1024_precomputed/dft.cpp
@@ -1,59 +1,27 @@
+#include<math.h>
 #include "dft.h"
-#include "coefficients1024.h"
+#include"coefficients1024.h"
 
 typedef int idx_t;
 
-void dft(DTYPE xx_R[SIZE], DTYPE xx_I[SIZE], DTYPE XX_R[SIZE], DTYPE XX_I[SIZE])
+void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE],DTYPE imag_op[SIZE])
 {
+	float w, c, s;
 
-	outer_for:
-	for (iter_t i = 0; i < SIZE; ++i) {
-#pragma HLS DATAFLOW
+	for (int i = 0; i < SIZE; i++) {
+#pragma HLS unroll
+		real_op[i] = 0;
+		imag_op[i] = 0;
+	}
 
-		DTYPE temp_R_out[SIZE];
-#pragma HLS ARRAY_MAP variable=temp_R_out instance=temp_merged horizontal
-		DTYPE temp_I_out[SIZE];
-#pragma HLS ARRAY_MAP variable=temp_I_out instance=temp_merged horizontal
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			int index = (j * i) % SIZE;
+			c = cos_coefficients_table[index];
+			s = sin_coefficients_table[index];
 
-		DTYPE c[SIZE];
-#pragma HLS ARRAY_MAP variable=c instance=cos_sin_merged horizontal
-		DTYPE s[SIZE];
-#pragma HLS ARRAY_MAP variable=s instance=cos_sin_merged horizontal
-
-		DTYPE sumR[SIZE], sumI[SIZE];
-
-		for1:
-		for (iter_t j = 0; j < SIZE; ++j) {
-#pragma HLS PIPELINE
-#pragma HLS UNROLL skip_exit_check factor=8
-
-			idx_t idx = (j * i) % SIZE;
-			c[j] = cos_coefficients_table[idx];
-			s[j] = sin_coefficients_table[idx];
+			real_op[i] += (real_sample[j] * c - imag_sample[j] * s);
+			imag_op[i] += (real_sample[j] * s + imag_sample[j] * c);
 		}
-
-		for2:
-		for (iter_t j = 0; j < SIZE; ++j) {
-#pragma HLS PIPELINE
-#pragma HLS UNROLL skip_exit_check factor=8
-
-			DTYPE c_temp = c[j];
-			DTYPE s_temp = s[j];
-			DTYPE temp_R_in = xx_R[j];
-			DTYPE temp_I_in = xx_I[j];
-			temp_R_out[j] = (temp_R_in * c_temp - temp_I_in * s_temp);
-			temp_I_out[j] = (temp_R_in * s_temp + temp_I_in * c_temp);
-		}
-
-		for3:
-		for(iter_t k = 0; k < SIZE; ++k) {
-#pragma HLS PIPELINE
-#pragma HLS UNROLL skip_exit_check factor=8
-
-			sumR[i] = (k == 0) ? temp_R_out[k] : (DTYPE)(sumR[i] + temp_R_out[k]);
-			sumI[i] = (k == 0) ? temp_I_out[k] : (DTYPE)(sumI[i] + temp_I_out[k]);
-		}
-		XX_R[i] = sumR[i];
-		XX_I[i] = sumI[i];
 	}
 }
