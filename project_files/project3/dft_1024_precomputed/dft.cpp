9becbfb7fabd9af8c2a52831bddb73cf3c6af84d
Soumil Paranjpay <soumil.paranjpay@gmail.com>
2025-11-08 21:37:30 -0800

    fix: q6

--- project_files/project3/dft_1024_precomputed/dft.cpp
+++ project_files/project3/dft_1024_precomputed/dft.cpp
@@ -2,26 +2,25 @@
 #include "dft.h"
 #include"coefficients1024.h"
 
-typedef int idx_t;
 
 void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE],DTYPE imag_op[SIZE])
 {
 	float w, c, s;
+	DTYPE tmp_real, tmp_imag;
 
 	for (int i = 0; i < SIZE; i++) {
-#pragma HLS unroll
-		real_op[i] = 0;
-		imag_op[i] = 0;
-	}
-
-	for (int i = 0; i < SIZE; i++) {
+		tmp_real = 0;
+		tmp_imag = 0;
 		for (int j = 0; j < SIZE; j++) {
 			int index = (j * i) % SIZE;
 			c = cos_coefficients_table[index];
 			s = sin_coefficients_table[index];
 
-			real_op[i] += (real_sample[j] * c - imag_sample[j] * s);
-			imag_op[i] += (real_sample[j] * s + imag_sample[j] * c);
+			tmp_real += (real_sample[j] * c - imag_sample[j] * s);
+			tmp_imag += (real_sample[j] * s + imag_sample[j] * c);
 		}
+
+		real_op[i] = tmp_real;
+		imag_op[i] = tmp_imag;
 	}
 }
